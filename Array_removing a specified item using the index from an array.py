e4238f5c30aadcbbfcf82e3f1ffbabc8e8a1cf2f
TheDarkifY <hasanww2mh@gmail.com>
2021-12-26 22:32:32 +0600

    Removing a specified item using the index from an array.
    Using .pop() function to remove an item to the list or array

---
+++ Array_removing a specified item using the index from an array.py
@@ -0,0 +1,6 @@
+# Removing an specified item using the index from an array
+# using .pop() function
+x = [1, 3, 5, 7, 9]
+x.pop(2)
+print(x)
+# print(x.remove(5), x) # mentioning the elements in array to removed
